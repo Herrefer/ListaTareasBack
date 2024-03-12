a862afc153fe05ea1158e71a3477d3a347547082
Fernando Herrera <herrera770@outlook.es>
2024-03-12 17:44:19 -0300

    se agregaron las funciones a la rutas principal para cada tipo de solicitud

--- src/routes/listaTareas.routes.js
+++ src/routes/listaTareas.routes.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { agregarTarea, borrarTarea, editarTarea, listarTareas } from "../controllers/listaTareas.controller.js";
 
 const enrutador = Router();
+
+enrutador.route('/').get(listarTareas).post(agregarTarea).delete(borrarTarea).patch(editarTarea);
+
+export default enrutador;
