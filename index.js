29509b98d4d52e46023b3131fe03bb388a1b4470
Fernando Herrera <herrera770@outlook.es>
2024-03-12 16:53:14 -0300

    se coniguraron los middlewares dotenv, cors y morgan. Tambien fueron configuradas los middlewares de express para interactuar con json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'dotenv/config';
 
 console.log('Todo va correcto. Nodemon funciona extraño pero se ejecuta (y)');
 
@@ -6,4 +7,8 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), () =>{
     console.log('Funciona correctamente el puerto adjudicado ' + app.get('port'))
-})
+});
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
