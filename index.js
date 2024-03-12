91f603fe47accd30a459b2440e7f09b161cd5bdf
Fernando Herrera <herrera770@outlook.es>
2024-03-12 17:18:11 -0300

    fué configurado un middleware para extablecer el index.js como página estática. Se creó listaTareas.routes.js y listaTareas.controller.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 
 console.log('Funciona todo correctamente (y)');
@@ -16,3 +18,10 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname= path.dirname(__filename);
+console.log(__dirname);
+app.use(express.static(path.join(__dirname,'/public')));
+
