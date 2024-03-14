74f4edb398451942ff5bd9b2a6e2133e03fb335d
Fernando Herrera <herrera770@outlook.es>
2024-03-14 13:17:08 -0300

    se agregó la funcion de buscar

--- src/controllers/listaTareas.controller.js
+++ src/controllers/listaTareas.controller.js
@@ -1,8 +1,15 @@
 import Tarea from "../database/model/tareas.js";
 
-export const listarTareas = (req, res) => {
-    console.log('la funcion listarTareas funciona');
-    res.send('tu solicitud para listar tareas fué enviada correctamente')
+export const listarTareas = async (req, res) => {
+    try{
+        const listaDeTareas = await Tarea.find();
+        console.log(listaDeTareas);
+        res.status(200).json(listaDeTareas);
+    }catch(error){
+        res.status(400).json({
+            mensaje: "no se pudo procesar la solicitud 'listarTareas'"
+        })
+    }
 };
 
 export const agregarTarea = async(req, res) => {
@@ -30,3 +37,15 @@ export const editarTarea = (req, res) => {
     console.log('la funcion editarTarea funciona');
     res.send('tu solicitud para editar tarea fué enviada correctamente')
 };
+
+export const buscarTarea = async (req,res) =>{
+    try{
+        const tareaEncontrada = await Tarea.findById(req.params.id);
+        console.log(tareaEncontrada);
+        res.status(200).json(tareaEncontrada);
+    }catch(error){
+        res.status(404).json({
+            mensaje: "no se pudo procesar la solicitud para buscarTarea"
+        })
+    }
+}
--- src/routes/listaTareas.routes.js
+++ src/routes/listaTareas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { agregarTarea, borrarTarea, editarTarea, listarTareas } from "../controllers/listaTareas.controller.js";
+import { agregarTarea, borrarTarea, buscarTarea, editarTarea, listarTareas } from "../controllers/listaTareas.controller.js";
 
 const enrutador = Router();
 
 enrutador.route('/').get(listarTareas).post(agregarTarea).delete(borrarTarea).patch(editarTarea);
+enrutador.route('/:id').get(buscarTarea);
 
 export default enrutador;
