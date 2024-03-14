d162046ff471b1cda16d04b10c2f00b16936c507
Fernando Herrera <herrera770@outlook.es>
2024-03-14 12:38:53 -0300

    se agregó la funcion para crear tarea desde el DB

--- src/controllers/listaTareas.controller.js
+++ src/controllers/listaTareas.controller.js
@@ -1,11 +1,24 @@
+import Tarea from "../database/model/tareas.js";
+
 export const listarTareas = (req, res) => {
     console.log('la funcion listarTareas funciona');
     res.send('tu solicitud para listar tareas fué enviada correctamente')
 };
 
-export const agregarTarea = (req, res) => {
-    console.log('la funcion agregarTarea funciona');
-    res.send('tu solicitud para agregar tarea fué enviada correctamente')
+export const agregarTarea = async(req, res) => {
+    try{
+        const tareaNueva = new Tarea (req.body);
+        await tareaNueva.save();
+        res.status(201).json({
+            mensaje: "Se ha creado una nueva tarea"
+        });
+        console.log(req.body);
+    }catch(error){
+        console.log(error);
+        res.status(400).json({
+            mensaje: "no se pudo procesar la solicitud 'agregarTarea'"
+        });
+    };
 };
 
 export const borrarTarea = (req, res) => {
